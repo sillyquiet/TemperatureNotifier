36c33a472135997a4246af52608d6cbe6b481c56
Matthew Riley <mathew.riley@gmail.com>
2018-06-24 11:40:14 -0500

    Playihg with react

---
+++ src/server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+const deviceFile = '/sys/bus/w1/devices/0117b2112cff/w1_slave';
+
+  `
+type Query {
+  hello: String
+}
+
+schema {
+  query: Query
+}`
+];
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(deviceFile)
+  });
+
+const resolvers = {
+  Query: {
+    hello(root) {
+        lineReader.on('line', function (line) {
+            console.log('Line from file:', line);
+          });
+    }
+  }
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const app = express();
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+app.listen(4000, () => console.log("Now browse to localhost:4000/graphiql"));
+``;
