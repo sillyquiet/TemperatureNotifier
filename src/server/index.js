9e3e7111c51d9251b4554f819ba8641e903f54ea
Matthew Riley <mathew.riley@gmail.com>
2018-06-25 21:04:57 -0500

    Return graphql promise on file read

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
 const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
 const { makeExecutableSchema } = require("graphql-tools");
-const deviceFile = "/sys/bus/w1/devices/28-0117b2112cff/w1_slave";
+const deviceName = "28-0117b2112cff";
 
 const typeDefs = [
-  `
+    `
 type Query {
-  hello: String
+  temperature: Float
+  timestamp: String
 }
 
 schema {
@@ -15,18 +18,57 @@ schema {
 }`
 ];
 
-const lineReader = require("readline").createInterface({
-  input: require("fs").createReadStream(deviceFile)
-});
+const FAHRENHEIT = "fahrenheit";
+const CELSIUS = "celsius";
+
+const readSensorHW = (unit = CELSIUS) => {
+    return new Promise((resolve, reject) => {
+        const sensor_path = `/sys/bus/w1/devices/${deviceName}/w1_slave`;
+        //const sensor_path = "src/server/sensorMock";
+
+        fs.access(sensor_path, fs.constants.R_OK, err => {
+            if (err) {
+                reject(`${sensor_path} cannot be accessed: ${err}`);
+            }
+            fs.readFile(sensor_path, { encoding: "utf8" }, function(err, data) {
+                if (err) {
+                    reject(err);
+                }
+                const lines = data.split("\n");
+                if (lines.length < 2) {
+                    reject(`Invalid sensor data: ${data}`);
+                }
+                if (!lines[0].match(/ YES$/)) {
+                    reject(`Checksum error reading sensor ${name}`);
+                }
+
+                const temp_pattern = /t=-?\d*/;
+                let temp_string = temp_pattern.exec(lines[1]);
+                if (temp_string === null) {
+                    reject(`Parsing error reading sensor ${name}`);
+                }
+                temp_string = String(temp_string).substr(2);
+                const temp = parseFloat(temp_string) / 1000;
+
+                if (unit === FAHRENHEIT) {
+                    resolve(temp * 1.8 + 32);
+                }
+
+                resolve(temp);
+            });
+        });
+    });
+};
 
 const resolvers = {
-  Query: {
-    hello(root) {
-      lineReader.on("line", function(line) {
-        return line;
-      });
+    Query: {
+        temperature(root) {
+            return readSensorHW();
+        },
+        timestamp(root) {
+            return Date.now();
+        }
     }
-  }
 };
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
